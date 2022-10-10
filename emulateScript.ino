1da72af323d54e4e810ead6be2dc9c18b9927d05
Martin SEBIROT <96467585+MarreTeint@users.noreply.github.com>
2022-10-10 17:26:55 +0200

    Add files via upload

--- emulateScript.ino
+++ emulateScript.ino
@@ -1,13 +1,15 @@
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial.begin(2000000);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   //while (Serial.available()==0){}
-    int out = rand()%99+1;
-    Serial.println(out);
-    //delay(1000);
+    int out = 23;
+    //rand()%99+1;
+    Serial.print(out);
+    Serial.print('\n');
+    delay(100);
 }
